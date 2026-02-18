07104597c57e523d110806e33f3dde85a4ec8c40
jorgette-aira <jorgetteairaamaro@gmail.com>
2026-02-18 17:22:45 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+N8N_WEBHOOK_URL = "https://jorgette.tail3679cb.ts.net/webhook-test/Workflow Checker"
+DEVOPS_ROLE_ID = "9988776655" # Use this if builder is not found
+
+USER_MAP = {
+    "Jorgette": "850349575486177322", # Your real Discord ID
+    "Allen": "323711023733538826",
+    "Neal": "1343136858577829898"
+}
