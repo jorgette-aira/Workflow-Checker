4b0b4a12d7b722f5cd29d018f398acf789051235
jorgette-aira <jorgetteairaamaro@gmail.com>
2026-02-19 13:17:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # config.py
 N8N_WEBHOOK_URL = "https://resolute-vikki-insightfully.ngrok-free.dev/webhook-test/Workflow-Checker"
-DEVOPS_ROLE_ID = "1473635058853085388" # Use this if builder is not found
+DEVOPS_ROLE_ID = "1473635058853085388" 
 
 USER_MAP = {
-    "Jorgette": "850349575486177322", # Your real Discord ID
+    # Discord ID
+    "Jorgette": "850349575486177322", 
     "Allen": "323711023733538826",
     "Neal": "1343136858577829898",
     "Default": DEVOPS_ROLE_ID
