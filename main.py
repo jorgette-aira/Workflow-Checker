a4d6e486c8cb38f12a854e558882a26c0165b218
jorgette-aira <jorgetteairaamaro@gmail.com>
2026-02-19 12:54:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
         with open(workflow_path, 'r') as f:
             workflow_data = json.load(f)
         
-        actual_agent_response = "Hello! I am your assistant from Batangas."
+        actual_agent_response = "Hello!"
         expected_qa_answer = "assistant Batangas" 
 
         # --- Performance Tracking Start ---
