853cf2a5a61fe8ed66607e25e032df3958d51001
jorgette-aira <jorgetteairaamaro@gmail.com>
2026-03-12 12:14:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 N8N_WEBHOOK_URL = "https://jorgette.tail3679cb.ts.net/webhook-test/Workflow-Checker"
-DEVOPS_ROLE_ID = "1473635058853085388" 
+DEVOPS_ROLE_ID = "1481086752348438679" 
 
 USER_MAP = {
     # Discord ID
